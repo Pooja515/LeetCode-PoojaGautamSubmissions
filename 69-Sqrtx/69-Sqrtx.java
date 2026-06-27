a6fb92d08f99a96011d6f21ed95af1333535fda1
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:04:54 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,16 @@
+// Last updated: 28/06/2026, 01:04:54
+class Solution {
+    public int mySqrt(int x) {
+        int low=1,high=x,ans=x;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if((long)mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+            }
+            else
+                 high=mid-1;
+        }
+        return ans;
+    }
+}
