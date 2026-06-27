5f348901043d5ea9f9fafe5e32c1bbf3cc6f22aa
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:01:54 +0530

    Create 2058-ConcatenationOfArray

---
+++ 2058-ConcatenationOfArray/2058-ConcatenationOfArray.java
@@ -0,0 +1,12 @@
+// Last updated: 28/06/2026, 01:01:54
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[2*n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+}
