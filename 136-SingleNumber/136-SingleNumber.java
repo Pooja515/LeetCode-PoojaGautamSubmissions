f08c7a45727165a2b8b53084abd3fcca0c6352d4
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:04:16 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 28/06/2026, 01:04:16
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor^=nums[i];
+        }
+        return xor;
+    }
+}
