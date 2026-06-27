7d09ac9e8fb59c8c0464b023bf0471f18e561cbf
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:03:53 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,20 @@
+// Last updated: 28/06/2026, 01:03:53
+class Solution {
+   
+    public int rob(int[] nums) {
+       int n = nums.length;
+       if(n==1) return nums[0]; 
+       int[] dp = new int[n];
+        
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+
+        for(int i=2 ;i<n;i++){
+             int pick = nums[i] + dp[i-2];
+             int notpick = 0 + dp[i-1];
+
+            dp[i] = Math.max(pick , notpick);
+        }
+   return dp[n-1];
+        }   
+}
