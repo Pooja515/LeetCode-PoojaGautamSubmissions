1a05a3578c8bcca70b49f371e0e08a217f154ce8
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:05:24 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,13 @@
+// Last updated: 28/06/2026, 01:05:24
+class Solution {
+    public int removeElement(int[] nums, int val) {
+       int i=0;
+       for(int j=0;j<nums.length;j++){
+       if(nums[j]!=val){
+            nums[i]=nums[j];
+            i++;
+         }
+       } 
+       return i;
+    }
+}
