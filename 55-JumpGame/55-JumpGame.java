20bb0af5b8ee9b0a1ec1aa602188abc32a07f366
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:05:05 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,12 @@
+// Last updated: 28/06/2026, 01:05:04
+class Solution {
+    public boolean canJump(int[] nums) {
+       int maxReach=0;
+       for(int i=0;i<nums.length;i++){
+        if(i>maxReach) return false;
+        maxReach=Math.max(maxReach,i+nums[i]);
+        if(maxReach>=nums.length-1) return true;
+       }
+       return true;
+    }
+}
