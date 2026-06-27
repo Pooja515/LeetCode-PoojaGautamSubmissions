60dbf44b0ddb66a2ce1f39c67f38941acf2bd70b
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:04:50 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,16 @@
+// Last updated: 28/06/2026, 01:04:50
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length, m= matrix[0].length;
+        int low= 0, high = m*n-1;
+        while(low <= high){
+            int mid = low + ( high - low ) / 2;
+            int row = mid/m, col = mid%m;
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) low=mid+1;
+            else
+                   high = mid-1;
+        }
+        return false;
+    }
+}
