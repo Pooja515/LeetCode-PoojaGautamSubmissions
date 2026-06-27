328c7b2c862c3469e56de05529556c5e3d8f6f5a
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:04:21 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,16 @@
+// Last updated: 28/06/2026, 01:04:21
+class Solution {
+    public boolean isPalindrome(String s) {
+      int n=s.length();
+      int l=0,r=n-1;
+      while(l<r){
+        while(l<r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+        while(l<r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+        if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r)))
+                 return false;
+        l++;
+        r--;
+      }  
+      return true;
+    }
+}
