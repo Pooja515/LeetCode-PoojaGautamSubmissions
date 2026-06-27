1b55fe7f64e4fa7d9fff53908865ef29153b8e15
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:05:16 +0530

    Create 40-CombinationSumIi

---
+++ 40-CombinationSumIi/40-CombinationSumIi.java
@@ -0,0 +1,22 @@
+// Last updated: 28/06/2026, 01:05:15
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        f(0,candidates,target,ans, new ArrayList<>());
+        return ans;
+    }
+    void f(int ind , int[] candidates, int target , List<List<Integer>> ans, List<Integer> ds){
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i = ind ; i<candidates.length ; i++){
+            if(i>ind && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i]> target) break;
+            ds.add(candidates[i]);
+            f(i+1 , candidates , target-candidates[i] , ans , ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
