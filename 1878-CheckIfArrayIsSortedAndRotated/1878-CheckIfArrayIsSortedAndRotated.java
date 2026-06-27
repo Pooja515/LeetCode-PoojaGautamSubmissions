d410383e074df5be492d2514c64af9f2611eb15e
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-28 01:02:01 +0530

    Create 1878-CheckIfArrayIsSortedAndRotated

---
+++ 1878-CheckIfArrayIsSortedAndRotated/1878-CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,13 @@
+// Last updated: 28/06/2026, 01:02:01
+class Solution {
+    public boolean check(int[] nums) {
+         int cnt = 0 , n= nums.length;
+         for(int i = 0 ;i < n; i++){
+              if(nums[i] >nums[(i+1) % n] ){
+                cnt++;
+              }
+         }
+         if(cnt > 1) return false;
+         return true;
+    }
+}
