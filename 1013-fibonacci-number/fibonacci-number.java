ba013960c8739aa72339c1e720f41a2d9529e499
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-29 02:24:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (33.00%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public int fib(int n) {
         if(n <= 1) return n;
-        
-        int[] dp = new int[n+1];
-        dp[0] =0;
-        dp[1] =1;
+
+        int prev2 =0 , prev1 =1;
 
         for(int i = 2 ;i <= n ; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int cur  = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
         }
-        return dp[n];  
+        return prev1;  
       }
 }
