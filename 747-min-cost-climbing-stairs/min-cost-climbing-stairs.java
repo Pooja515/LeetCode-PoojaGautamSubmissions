bae7f0f4f1b85e3af4428bf18ecfe39781d90463
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-06-29 08:15:35 +0530

    Time: 1 ms (39.42%) | Memory: 45 MB (23.10%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    int[] memo;
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        if(n<=1) return 0;
+
+        memo = new int[n+1];
+        Arrays.fill(memo , -1);
+
+        return f(n,cost);
+    }
+    int f(int n , int[] cost){
+        if(n <= 1) return 0;
+
+        if(memo[n] != -1) return memo[n];
+        int choice1 = f(n-1 , cost) + cost[n-1];
+        int choice2 = f(n-2 , cost ) + cost[n-2];
+
+        return memo[n] = Math.min(choice1 , choice2);
+    }
+}
