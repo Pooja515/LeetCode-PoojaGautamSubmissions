c5751984d3623d57ee60d13149aed4a73581e7e2
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-02 10:38:13 +0530

    Time: 19 ms (72.05%) | Memory: 54.2 MB (64.89%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    int[][] memo ;
+    public int numDistinct(String s, String t) {
+        int n = s.length() , m = t.length();
+        
+        memo = new int[n][m];
+        for(int[] row : memo){
+            Arrays.fill(row , -1);
+        }
+
+        return f(n-1 , m-1 , s , t);
+    }
+    int f(int i , int j , String s , String t){
+        if(j< 0) return 1;
+        if(i<0 ) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        if(s.charAt(i) == t.charAt(j)){
+            return memo[i][j] =f(i-1 , j-1 , s , t) + f(i-1 , j , s , t);
+        }
+        return memo[i][j] = f(i-1 , j , s , t);
+    }
+}
