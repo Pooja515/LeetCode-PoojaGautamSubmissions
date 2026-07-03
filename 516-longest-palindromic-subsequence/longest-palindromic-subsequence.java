3be42e86cedf897fe65949889fc9bfe2f500288b
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-03 16:14:21 +0530

    Time: 67 ms (8.50%) | Memory: 64 MB (45.36%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -2,47 +2,26 @@ class Solution {
   
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
-
-        String s2 =  new StringBuilder(s).reverse().toString();
-
-        
-        int m = s2.length();
-
-        int[] dp = new int[m+1];
+        int[][] dp = new int[n+1][n+1];
 
         for(int i = 0;i<=n;i++){
-              int[] cur = new int[m+1];
-            for(int j=0;j<=m;j++){
+            for(int j=0;j<=n;j++){
 
                  if(i == 0 || j == 0){
-                    cur[j] =0;
+                    dp[i][j] =0;
                  }
 
                  else{
-                       if(s.charAt(i-1) == s2.charAt(j-1)){
-                              cur[j] = 1 + dp[j-1];
+                      if (s.charAt(i - 1) == s.charAt(n - j)) {
+                              dp[i][j] = 1 + dp[i-1][j-1];
                         }
                         else{
-                             cur[j] = 0 + Math.max(dp[j] , cur[j-1] );
+                             dp[i][j] = 0 + Math.max(dp[i-1][j] , dp[i][j-1] );
                           } 
                     }
                     
                 }
-                dp = cur;
             }
-      return dp[m];
+      return dp[n][n];
     }
-    /** 
-    String reverse(String s , int l , int r){
-        char[] a = s.toCharArray();
-        while(l<r){
-        char temp = a[l];
-        a[l] = a[r];
-        a[r] = temp;
-        l++;
-        r--;
-       }
-       return new String(a);
-    }
-    */
 }
