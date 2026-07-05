c557a8f6bb20b0893ef089101f9bf3792bdb712f
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-05 15:36:40 +0530

    Time: 54 ms (37.85%) | Memory: 118.2 MB (26.28%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-// Size 4 for cap because cap can be 0, 1, 2, or 3
-int[][][] dp = new int[n + 1][2][4]; 
+        if(n == 0) return 0;
+  
+        int[][][] dp = new int[n + 1][2][3]; 
 
 for (int i = n - 1; i >= 0; i--) {
     for (int buy = 0; buy < 2; buy++) {
         // cap needs to go down to 1 because cap-1 is used below
-        for (int cap = 3; cap >= 1; cap--) { 
+        for (int cap = 2; cap >=1 ; cap--) { 
             if (buy == 1) {
                 int take = -prices[i] + dp[i + 1][0][cap];
                 int nottake = 0 + dp[i + 1][1][cap];
