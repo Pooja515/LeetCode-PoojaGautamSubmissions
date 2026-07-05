77dfb5f9d13aa579cb372233d0bb0372248cbb1c
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-05 15:42:42 +0530

    Time: 65 ms (29.53%) | Memory: 117.9 MB (31.87%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -6,23 +6,20 @@ class Solution {
         int[][][] dp = new int[n + 1][2][3]; 
 
 for (int i = n - 1; i >= 0; i--) {
-    for (int buy = 0; buy < 2; buy++) {
-        // cap needs to go down to 1 because cap-1 is used below
-        for (int cap = 2; cap >=1 ; cap--) { 
+    for (int buy = 0; buy <= 1; buy++) {
+        for (int cap = 1; cap <=2 ; cap++) { 
             if (buy == 1) {
                 int take = -prices[i] + dp[i + 1][0][cap];
                 int nottake = 0 + dp[i + 1][1][cap];
                 dp[i][buy][cap] = Math.max(take, nottake);
             } else {
-                int take = prices[i] + dp[i + 1][1][cap - 1]; // Fixed buy state to 1 after selling
+                int take = prices[i] + dp[i + 1][1][cap - 1]; 
                 int nottake = 0 + dp[i + 1][0][cap];
                 dp[i][buy][cap] = Math.max(take, nottake);
             }
         }
     }
 }
-
-// The result will be stored here (starting at day 0, looking to buy, with 2 transactions left)
 return dp[0][1][2];
     }
 }
