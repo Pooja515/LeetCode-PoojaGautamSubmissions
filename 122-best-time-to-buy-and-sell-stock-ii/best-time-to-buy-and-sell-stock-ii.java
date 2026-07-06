fb5ac359c73f4b1302ffcb89846092a6bfbb9068
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-06 13:24:02 +0530

    Time: 4 ms (10.68%) | Memory: 50.4 MB (5.36%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     int[][] memo;
+
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        
+
         memo = new int[n][2];
-        for(int[] row : memo){
-            Arrays.fill(row , -1);
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
         }
         return f(0, 1, prices, n);
     }
@@ -14,13 +15,14 @@ class Solution {
         //BASE CASE 
         if (ind == n)
             return 0;
-        
-        if(memo[ind][buy] != -1) return memo[ind][buy];
+
+        if (memo[ind][buy] != -1)
+            return memo[ind][buy];
 
         if (buy == 1) {
             int buytoday = -prices[ind] + f(ind + 1, 0, prices, n);
             int notBuytoday = 0 + f(ind + 1, 1, prices, n);
-            return  memo[ind][buy] = Math.max(buytoday, notBuytoday);
+            return memo[ind][buy] = Math.max(buytoday, notBuytoday);
 
         } else {
             int selltoday = prices[ind] + f(ind + 1, 1, prices, n);
