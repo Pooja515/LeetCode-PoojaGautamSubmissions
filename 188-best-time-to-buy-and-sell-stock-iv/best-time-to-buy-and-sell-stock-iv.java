5dbf82f418f31cb996a62302df21681426e09ca5
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-08 18:44:36 +0530

    Time: 2 ms (72.16%) | Memory: 45.4 MB (29.75%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -2,11 +2,13 @@ class Solution {
     public int maxProfit(int k, int[] prices) {
        int n = prices.length;
        int[][] dp = new int[2][k+1]; 
-       for(int ind = n-1 ;ind >= 0 ;ind--){
-        for(int trans = 1 ;trans <= k;trans++){
-            dp[0][trans] = Math.max(prices[ind] + dp[1][trans-1] , 0+ dp[0][trans]);
-            dp[1][trans] = Math.max(-prices[ind] + dp[0][trans] , 0+dp[1][trans]);
+       for(int i =n-1 ;i>= 0 ;i--){
+        int[][] cur = new int[2][k+1]; 
+        for(int j= 1 ;j<=k;j++){
+            cur[0][j] = Math.max(prices[i] + dp[1][j-1] , 0+ dp[0][j]);
+            cur[1][j] = Math.max(-prices[i] + dp[0][j] , 0+dp[1][j]);
         }
+        dp = cur;
        }
        return dp[1][k];
     }
