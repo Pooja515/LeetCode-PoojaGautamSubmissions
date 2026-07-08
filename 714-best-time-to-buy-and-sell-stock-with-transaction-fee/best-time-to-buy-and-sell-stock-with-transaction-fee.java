3465ba26d9b10d50d477828c4e47040d68a8e117
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-08 23:39:06 +0530

    Time: 20 ms (37.93%) | Memory: 70.9 MB (38.16%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-          int n = prices.length;
+        int n = prices.length;
         int[][] dp = new int[n+1][2];
-        
-        for(int i= n-1 ;i >= 0;i--){
-            dp[i][0] = Math.max(prices[i] - fee + dp[i+1][1] ,dp[i+1][0]);
-            dp[i][1] = Math.max(-prices[i] + dp[i+1][0] ,dp[i+1][1]);
+        for(int ind = n-1 ; ind >= 0 ;ind--){
+            //sell
+            dp[ind][0] = Math.max(prices[ind] + dp[ind+1][1] - fee , 0 + dp[ind+1][0]);
+            //buy
+            dp[ind][1] = Math.max(-prices[ind] + dp[ind+1][0] , 0 + dp[ind+1][1]);
         }
-        return dp[0][1]; 
+        return dp[0][1];
     }
 }
-
