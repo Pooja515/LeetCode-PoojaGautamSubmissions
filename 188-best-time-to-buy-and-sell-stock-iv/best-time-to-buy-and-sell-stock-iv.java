82cf86a01751b2a05feea4ab3fe913d5e2a4256a
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-08 18:24:47 +0530

    Time: 9 ms (19.95%) | Memory: 45.2 MB (46.12%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,13 +1,32 @@
 class Solution {
+    int[][][] memo;
     public int maxProfit(int k, int[] prices) {
-       int n = prices.length;
-       int[][] dp = new int[2][k+1]; 
-       for(int ind = n-1 ;ind >= 0 ;ind--){
-        for(int trans = 1 ;trans <= k;trans++){
-            dp[0][trans] = Math.max(prices[ind] + dp[1][trans-1] , 0+ dp[0][trans]);
-            dp[1][trans] = Math.max(-prices[ind] + dp[0][trans] , 0+dp[1][trans]);
+        int n = prices.length;
+        memo = new int[n][2][k+1];
+        for(int[][] row : memo){
+            for(int r[] : row){
+                Arrays.fill(r , -1);
+            }
         }
-       }
-       return dp[1][k];
+
+        return f(0,1,k, prices,n);
+    }
+    int f(int ind ,int buy, int k , int[] prices, int n){
+        if(ind == n || k == 0 ) return 0;
+
+        if(memo[ind][buy][k] != -1){
+            return memo[ind][buy][k];
+        }
+
+        if(buy == 1){
+            int buyToday = -prices[ind] + f(ind+1 , 0 , k, prices , n);
+            int skipBuy = 0 + f(ind+1 , 1 , k , prices ,n);
+            return memo[ind][buy][k] =Math.max(buyToday,skipBuy);
+        }
+        else{
+             int sellToday = prices[ind] + f(ind+1 , 1 , k-1, prices , n);
+            int holdSell = 0 + f(ind+1 , 0 , k , prices ,n);
+            return memo[ind][buy][k] = Math.max(sellToday,holdSell);
+        }
     }
 }
