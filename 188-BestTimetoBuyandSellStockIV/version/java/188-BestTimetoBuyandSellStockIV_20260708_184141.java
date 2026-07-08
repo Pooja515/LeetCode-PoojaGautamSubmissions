8c70f14bd7f3c1a7529db09b0baad70d1376c07f
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-08 18:41:42 +0530

    Create 188-BestTimetoBuyandSellStockIV

---
+++ 188-BestTimetoBuyandSellStockIV/version/java/188-BestTimetoBuyandSellStockIV_20260708_184141.java
@@ -0,0 +1,14 @@
+// Last updated: 08/07/2026, 18:41:41
+1class Solution {
+2    public int maxProfit(int k, int[] prices) {
+3       int n = prices.length;
+4       int[][] dp = new int[2][k+1]; 
+5       for(int ind = n-1 ;ind >= 0 ;ind--){
+6        for(int trans = 1 ;trans <= k;trans++){
+7            dp[0][trans] = Math.max(prices[ind] + dp[1][trans-1] , 0+ dp[0][trans]);
+8            dp[1][trans] = Math.max(-prices[ind] + dp[0][trans] , 0+dp[1][trans]);
+9        }
+10       }
+11       return dp[1][k];
+12    }
+13}
