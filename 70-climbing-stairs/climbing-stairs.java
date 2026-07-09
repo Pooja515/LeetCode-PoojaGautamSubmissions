40f7dd4ce2c56c718d41342268f4a0eca9056b67
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-09 22:41:57 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,15 +3,15 @@ class Solution {
     public int climbStairs(int n) {
         if(n <= 1) return 1;
         if(n==2) return n;
-        int[] dp = new int[n+1] ;
-    
-         dp[0] = 1;
-         dp[1] = 1;
+        
+         int prev2 = 1 , prev1 = 1;
 
          for(int i = 2;i<=n ;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int cur  = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = cur;
          }
 
-        return dp[n];
+        return prev1;
     }
 }
