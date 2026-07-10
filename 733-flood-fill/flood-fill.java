12a1605cb4a7451a067d18e1cc4e663ebc8ef21f
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-11 00:41:17 +0530

    Time: 2 ms (12.15%) | Memory: 46.7 MB (62.03%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[][] dir = {{-1,0},{1,0} , {0,1} , {0,-1}};
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int original = image[sr][sc];
+        if(original == color) return image;
+        int m= image.length , n = image[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+        image[sr][sc] = color;
+        q.offer(new int[] {sr,sc});
+        visited[sr][sc] =true;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            for(int[] d : dir){
+                int newr = cur[0] + d[0];
+                int newc = cur[1] + d[1];
+                if(newr >= 0 && newr < m && newc >= 0 && newc < n && image[newr][newc] == original && !visited[newr][newc]){
+                    visited[newr][newc] = true;
+                    image[newr][newc] = color;
+                    q.add(new int[] {newr,newc});
+                }
+            }
+        }
+        return image;
+    }
+}
