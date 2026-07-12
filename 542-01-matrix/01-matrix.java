1849c0ae574e0008228614a2bb40364844dd361b
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-12 19:11:47 +0530

    Time: 14 ms (70.92%) | Memory: 49.1 MB (58.87%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,33 +1,33 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
 
-        if(mat == null || mat.length == 0) return mat;
+        if (mat == null || mat.length == 0)
+            return mat;
 
-        int m = mat.length , n = mat[0].length;
+        int m = mat.length, n = mat[0].length;
 
         Queue<int[]> q = new LinkedList<>();
-        
-        for(int r = 0 ; r<m ; r++){
-            for(int c = 0 ; c<n ; c++){
-                if(mat[r][c] == 0) {
-                    q.offer(new int[] {r,c});
-                }
-                else{
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] == 0) {
+                    q.offer(new int[] { r, c });
+                } else {
                     mat[r][c] = -1;
                 }
             }
         }
 
-        int[][] dir ={{-1,0} ,{1,0},{0,-1} ,{0,1}};
+        int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int[] cur = q.poll();
-            for(int[] d : dir){
+            for (int[] d : dir) {
                 int newr = cur[0] + d[0];
                 int newc = cur[1] + d[1];
-                if(newr >= 0 && newr < m && newc >= 0 && newc < n && mat[newr][newc] == -1){
+                if (newr >= 0 && newr < m && newc >= 0 && newc < n && mat[newr][newc] == -1) {
                     mat[newr][newc] = mat[cur[0]][cur[1]] + 1;
-                    q.offer(new int[] {newr,newc});
+                    q.offer(new int[] { newr, newc });
                 }
             }
 
