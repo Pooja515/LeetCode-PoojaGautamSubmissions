98d0d119bf3e39707ffaf32f393d655f29da498e
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-12 19:11:40 +0530

    Create 542-01Matrix

---
+++ 542-01Matrix/version/java/542-01Matrix_20260712_191140.java
@@ -0,0 +1,38 @@
+// Last updated: 12/07/2026, 19:11:40
+1class Solution {
+2    public int[][] updateMatrix(int[][] mat) {
+3
+4        if (mat == null || mat.length == 0)
+5            return mat;
+6
+7        int m = mat.length, n = mat[0].length;
+8
+9        Queue<int[]> q = new LinkedList<>();
+10
+11        for (int r = 0; r < m; r++) {
+12            for (int c = 0; c < n; c++) {
+13                if (mat[r][c] == 0) {
+14                    q.offer(new int[] { r, c });
+15                } else {
+16                    mat[r][c] = -1;
+17                }
+18            }
+19        }
+20
+21        int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+22
+23        while (!q.isEmpty()) {
+24            int[] cur = q.poll();
+25            for (int[] d : dir) {
+26                int newr = cur[0] + d[0];
+27                int newc = cur[1] + d[1];
+28                if (newr >= 0 && newr < m && newc >= 0 && newc < n && mat[newr][newc] == -1) {
+29                    mat[newr][newc] = mat[cur[0]][cur[1]] + 1;
+30                    q.offer(new int[] { newr, newc });
+31                }
+32            }
+33
+34        }
+35        return mat;
+36    }
+37}
