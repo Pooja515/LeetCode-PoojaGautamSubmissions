aa45f6494c81d85754c5fff2208d107adb3ee6cb
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-12 17:32:31 +0530

    Time: 2 ms (91.21%) | Memory: 44.2 MB (57.11%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,12 +1,13 @@
 class Solution {
-    int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
-
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid[0].length == 0)
+        if (grid == null || grid.length == 0)
             return 0;
+
         int m = grid.length, n = grid[0].length;
-        int fresh = 0;
-        Queue<int[]> q = new ArrayDeque<>();
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int minutes = 0, fresh = 0;
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 if (grid[r][c] == 2) {
@@ -17,22 +18,23 @@ class Solution {
                 }
             }
         }
-        int minutes = 0;
+        int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
         while (!q.isEmpty() && fresh > 0) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
                 int[] cur = q.poll();
 
-                for (int[] d : dir) {
-                    int newr = cur[0] + d[0];
-                    int newc = cur[1] + d[1];
+                for (int[] d : dirs) {
+                    int newr = d[0] + cur[0];
+                    int newc = d[1] + cur[1];
+
                     if (newr >= 0 && newr < m && newc >= 0 && newc < n && grid[newr][newc] == 1) {
                         fresh--;
-
                         grid[newr][newc] = 2;
                         q.offer(new int[] { newr, newc });
                     }
                 }
+
             }
             minutes++;
         }
