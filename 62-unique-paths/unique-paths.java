23f388b48e8e34a092b66211bc56aa9f4161e7c2
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-15 00:45:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (29.15%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,25 +4,28 @@ class Solution {
         if (m == 0)
             return 0;
 
-        int[][] dp = new int[m][n];
+        int[] dp = new int[n];
 
         for (int i = 0; i < m; i++) {
+            int[] cur = new int[n];
+
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
+                    cur[j] = 1;
                     continue;
                 }
 
                 int left = 0, up = 0;
                 if (i > 0)
-                    up = dp[i - 1][j];
+                    up = dp[j];
 
                 if (j > 0)
-                    left = dp[i][j - 1];
+                    left = cur[j - 1];
 
-                dp[i][j] = left + up;
+                cur[j] = left + up;
             }
+            dp = cur;
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
