3c871b3df50efc794a2c99e240c7ebb9686d842f
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-15 16:53:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (80.94%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -9,28 +9,30 @@ class Solution {
         if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
             return 0;
 
-        int[][] dp = new int[m][n];
+        int[] dp = new int[n];
 
         for (int i = 0; i < m; i++) {
+               int[] cur = new int[n];
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) {
-                      dp[i][j] =1;
+                      cur[j] =1;
                       continue;
                 }
                 
                 if (obstacleGrid[i][j] == 1){
-                    dp[i][j] =  0;
+                    cur[j] =  0;
                     continue;
                 }
                 
 
                 int up = 0 , left = 0;
-                if(i>0) up =   dp[i - 1][j];
-                if(j>0) left = dp[i][j - 1];
+                if(i>0) up =   dp[j];
+                if(j>0) left = cur[j - 1];
 
-                dp[i][j] = up + left;
+                cur[j] = up + left;
             }
+            dp = cur;
         }
-       return dp[m-1][n-1];
+       return dp[n-1];
     }
 }
