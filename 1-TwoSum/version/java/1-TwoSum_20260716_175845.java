7fd3655b3fb4a955111354e770fccf11bee343e9
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-16 17:58:46 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/java/1-TwoSum_20260716_175845.java
@@ -0,0 +1,23 @@
+// Last updated: 16/07/2026, 17:58:45
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if (s.length() != t.length()) {
+4            return false;
+5        }
+6
+7        int[] freq = new int[26];
+8
+9        for (int i = 0; i < s.length(); i++) {
+10            freq[s.charAt(i) - 'a']++;
+11            freq[t.charAt(i) - 'a']--;
+12        }
+13
+14        for (int count : freq) {
+15            if (count != 0) {
+16                return false;
+17            }
+18        }
+19
+20        return true;
+21    }
+22}
