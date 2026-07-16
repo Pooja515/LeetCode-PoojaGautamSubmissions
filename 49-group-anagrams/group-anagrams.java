042212d1d3c234d66fec52895ffcf2f7b76b3ecc
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-16 19:04:23 +0530

    Time: 6 ms (99.54%) | Memory: 49.5 MB (84.40%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            char[] a = str.toCharArray();
+
+            Arrays.sort(a);
+
+            String key = String.valueOf(a);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
