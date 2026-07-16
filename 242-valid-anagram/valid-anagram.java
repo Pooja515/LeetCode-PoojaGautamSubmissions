a01e6c639a30e1ba61f796467c46aee611f96824
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-16 17:58:41 +0530

    Time: 6 ms (43.39%) | Memory: 44.4 MB (74.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int m = s.length(), n = t.length();
-        if (n != m)
+        if (s.length() != t.length()) {
             return false;
+        }
 
         int[] freq = new int[26];
 
-        for (int i = 0; i < m; i++) {
-          
+        for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
             freq[t.charAt(i) - 'a']--;
+        }
 
-        }
-    
-        for (int it : freq) {
-            if (it != 0)
+        for (int count : freq) {
+            if (count != 0) {
                 return false;
+            }
         }
+
         return true;
     }
 }
