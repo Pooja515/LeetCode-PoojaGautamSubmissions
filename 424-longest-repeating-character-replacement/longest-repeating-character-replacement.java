f65f70a8f05773d21775cbe223ea543609fabf98
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-20 08:34:25 +0530

    Time: 28 ms (23.27%) | Memory: 47.5 MB (7.39%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,7 +5,7 @@ class Solution {
         for(int r=0 ; r<s.length() ; r++){
             map.put(s.charAt(r) , map.getOrDefault(s.charAt(r),0)+1);
             maxfreq = Math.max(maxfreq , map.get(s.charAt(r)));
-            if((r-l+1) - maxfreq >k){
+            while((r-l+1) - maxfreq >k){
                 map.put(s.charAt(l) , map.get(s.charAt(l))-1);
                 if(s.charAt(l) == 0) map.remove(s.charAt(l));
                 l++;
