fe8e2f8bbbc17e621d22b863e2d4f9ac54854b55
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-22 07:43:56 +0530

    Time: 111 ms (9.31%) | Memory: 72.5 MB (33.47%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -13,21 +13,23 @@ class Solution {
         memo = new Boolean[n][target + 1];
 
        
-        return f(n-1 , nums, target);
+        return f(0 , nums, target);
     }
 
     boolean f(int i , int[] nums, int target){
         if(target == 0) return true;
-        if(i == 0)
-           return (nums[i] == target);
+        if(i == nums.length)
+           return false;
           if (memo[i][target] != null) {
             return memo[i][target];
         }
-        boolean nottake = f(i-1 , nums , target);
+        boolean nottake = f(i+1 , nums , target);
         boolean take = false;
         if(nums[i] <= target){
-            take = f(i-1 , nums , target - nums[i]);
+            take = f(i+1 , nums , target - nums[i]);
         }
          return memo[i][target] =  nottake || take;
     }
 }
+
+
