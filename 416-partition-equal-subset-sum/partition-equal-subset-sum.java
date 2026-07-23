f2f15d012a82d06bcfd3998da876852c8299e742
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-24 03:55:34 +0530

    Time: 51 ms (44.81%) | Memory: 47.8 MB (46.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    Boolean[][] memo ;
+
     public boolean canPartition(int[] nums) {
       int n = nums.length;
       int totalsum = 0;
@@ -8,25 +8,31 @@ class Solution {
       }
       if(totalsum % 2 != 0) return false; 
       int target = totalsum/2; 
-      memo = new Boolean[n][target+1];
-      return f(n-1,target , nums);
-    }
-
-    boolean f(int ind , int target , int[] nums){
-        if(target == 0) return true;
-        if(ind == 0){
-          
-                return nums[0] == target;
-            
+       boolean[][] dp = new boolean[n][target+1];
+     
+        for(int i = 0 ; i<n ; i++){
+            dp[i][0] = true;
         }
-        if(memo[ind][target] != null) return memo[ind][target];
-        boolean notpick = f(ind-1 , target , nums);
-        boolean pick = false;
-        if(nums[ind] <= target){
-            pick = f(ind-1 , target - nums[ind] , nums);
+        if(n == 1){
+            if(nums[0] <= target){
+               dp[0][nums[0]] = true;
+            }
+        }
+        
+        
+        for(int ind = 1 ;ind < n ;ind++){
+            for(int t =1; t<= target;t++){
+                boolean notpick = dp[ind-1][t];
+                boolean pick = false;
+                if(nums[ind] <= t){
+                    pick = dp[ind-1][t - nums[ind]];
+                }
+
+                dp[ind][t] = pick || notpick;
+            }
         }
 
-        return memo[ind][target] = pick || notpick;
+        return dp[n-1][target];
     }
 
 }
