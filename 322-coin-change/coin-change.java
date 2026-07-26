6728a1b94e8bbb689e5353b2ceb8d9f9b97e25de
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-26 09:18:30 +0530

    Time: 16 ms (68.16%) | Memory: 47.1 MB (20.05%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,34 +1,30 @@
 class Solution {
-    int[][] memo;
+  
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
-        memo = new int[n][amount + 1];
-        for(int[] rows : memo){
-            Arrays.fill(rows , -1);
-        }
-        int res = f(n - 1, amount, coins);
-        return res >= (int) 1e9 ? -1 : res;
-    }
-
-    int f(int ind, int amount, int[] coins) {
-        if (amount == 0) {
-            return 0;
+        int[][] dp = new int[n][amount + 1];
+        
+        for(int target = 0 ;target <=amount ;target++){
+            if(target % coins[0] == 0){
+                 dp[0][target] = target/coins[0];
+            }
+            else
+                 dp[0][target] = (int) 1e9;
+           
         }
 
-        if (ind == 0) {
-            if (amount % coins[0] == 0) {
-                return amount / coins[0];
-            } else
-                return (int) 1e9;
-        }
-        if(memo[ind][amount] != -1) return memo[ind][amount];
-        int notpick = 0 + f(ind - 1, amount, coins);
-        int pick = (int) 1e9;
-        if (coins[ind] <= amount) {
-            pick = 1 + f(ind, amount - coins[ind], coins);
-        }
+        for(int ind = 1;ind < n ;ind++){
+            for(int target = 0 ; target <= amount ;target++){
+            int notpick = 0 + dp[ind - 1][target];
+            int pick = (int) 1e9;
+            if (coins[ind] <= target) {
+                pick = 1 + dp[ind][target - coins[ind]];
+            }
 
-        return memo[ind][amount] =  Math.min(pick, notpick);
+            dp[ind][target] =  Math.min(pick, notpick);
+            }
+        }
+        return dp[n-1][amount] >= (int) 1e9 ?-1 : dp[n-1][amount];
     }
 }
