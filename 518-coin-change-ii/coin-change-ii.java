70959fabf5faff3156860c216f42d8ae62626737
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-26 10:45:01 +0530

    Time: 11 ms (60.52%) | Memory: 57.2 MB (22.37%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,30 +1,31 @@
 class Solution {
-    int[][] memo;
+    
     public int change(int amount, int[] coins) {
      int  n = coins.length;
-     memo = new int[n][amount + 1];
-     for(int[] rows : memo){
-        Arrays.fill(rows, -1);
-     }
-     return f(n-1 , amount , coins);   
-    }
-    int f(int ind , int amount ,int[] coins){
+     int[][] dp = new int[n][amount + 1];
 
-        if(amount == 0) return 1;
-        if(ind == 0){
-            if(coins[ind] <= amount){
-                return amount % coins[0] == 0 ? 1:0;
+     for(int target = 0 ;target <= amount ;target++){
+            if(target % coins[0] ==0){
+                  dp[0][target] = 1;
             }
             else
-                  return 0;
+                    dp[0][target] = 0;
         }
-        if(memo[ind][amount] != -1) return memo[ind][amount];
-        int notpick = f(ind-1 , amount , coins);
-        int pick = 0;
-        if(coins[ind] <= amount){
-            pick = f(ind , amount - coins[ind] , coins);
+        
+     
+        for(int ind = 1 ;ind <n ;ind++){
+            for(int target =0 ; target <= amount ;target++){
+                int notpick = dp[ind-1][target];
+                int pick = 0;
+                if(coins[ind] <= target){
+                    pick = dp[ind][target - coins[ind]];
 
+                }
+                dp[ind][target] = pick + notpick;
+            }
         }
-        return memo[ind][amount] = pick + notpick;
+      
+      return dp[n-1][amount];
+      
     }
 }
