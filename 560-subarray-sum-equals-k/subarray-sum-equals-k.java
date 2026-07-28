aecd2d5ff0061082ce27b28fcfdaf2e7074ca6b7
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-29 03:07:32 +0530

    Time: 24 ms (76.55%) | Memory: 48.6 MB (78.99%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,7 +1,7 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
+        map.put(0, 1); 
 
         int rightsum = 0 ,count = 0;
 
