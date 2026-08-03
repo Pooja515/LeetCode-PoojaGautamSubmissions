35573acc55e17139b392c34b19c176f0d12e1c77
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-03 21:18:35 +0530

    Create 1143-LongestCommonSubsequence

---
+++ 1143-LongestCommonSubsequence/version/java/1143-LongestCommonSubsequence_20260803_211834.java
@@ -0,0 +1,25 @@
+// Last updated: 03/08/2026, 21:18:34
+1class Solution {
+2    
+3    public int longestCommonSubsequence(String text1, String text2) {
+4        int m = text1.length(), n = text2.length();
+5
+6        int[][] dp = new int[m+1][n+1];
+7
+8        for(int ind1 =0; ind1<=m; ind1++){
+9            for(int ind2 =0; ind2<=n; ind2++){
+10               if(ind1==0 || ind2==0){
+11                   dp[ind1][ind2] =0;
+12               }
+13               else{
+14                    if (text1.charAt(ind1-1) == text2.charAt(ind2-1)) {
+15                         dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+16                    } else {
+17                         dp[ind1][ind2] = 0 + Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+18                    }
+19               }
+20            }
+21        }
+22          return dp[m][n];
+23    }
+24}
