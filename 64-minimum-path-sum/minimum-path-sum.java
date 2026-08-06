3bbcdee7946ce615c8b84a80c0c2f02c54f6adfb
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-06 21:35:50 +0530

    Time: 5 ms (25.43%) | Memory: 50.3 MB (39.01%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,31 +1,35 @@
 class Solution {
-    int[][] memo;
+
     public int minPathSum(int[][] grid) {
-        int m = grid.length , n = grid[0].length;
-        if(m==0 || n==0) return 0;
-        memo = new int[m][n];
-        for(int[] rows : memo){
-            Arrays.fill(rows,-1);
+        int m = grid.length, n = grid[0].length;
+        if (m == 0 || n == 0)
+            return 0;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    if (i < 0 || j < 0)
+                        dp[i][j] = (int) 1e9;
+                    else {
+                        int left = (int) 1e9;
+                        int up = (int) 1e9;
+                        if (i > 0) {
+                            up = dp[i - 1][j];
+                        }
+                        if (j > 0) {
+                            left = dp[i][j - 1];
+                        }
+
+                        dp[i][j] = grid[i][j] + Math.min(up, left);
+                    }
+
+                }
+            }
         }
 
-        return f(m-1,n-1,grid) == (int) 1e9 ? -1:f(m-1,n-1,grid);
-    }
-    int f(int i, int j , int[][] grid){
-        //base case
-        if(i==0 && j==0) return grid[0][0];
-        if(i<0 || j<0) return (int) 1e9;
-        int left= (int) 1e9;
-        int up = (int) 1e9;
-
-        if(memo[i][j] != -1) return memo[i][j];
-        if(i>0) {
-            up = f(i-1,j,grid);
-        }
-        if(j>0){
-            left = f(i,j-1,grid);
-        }
-
-       memo[i][j] = grid[i][j] + Math.min(up , left);
-       return memo[i][j];
+        return dp[m-1][n-1];
     }
 }
