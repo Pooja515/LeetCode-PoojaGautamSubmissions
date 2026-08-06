1d45d16a6e6f7fbd5e6d31952aef9dcce080214a
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-06 16:50:26 +0530

    Time: 11 ms (52.18%) | Memory: 51.3 MB (16.39%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -25,69 +25,38 @@ class Solution {
             if(nums[i] % d != 0){
                 sum += 1;
             }
+             if(sum > threshold) return false;
         }
-        return sum <= threshold;
+        return true;
     }
 }
+
 /** 
-    Binary search
-    based on answers:
-    monotonic incr
-    or monotonic decr
+    Binary search  based on answers:monotonic incr or monotonic decr
 
-    ==>
-    Find the
-    smallest divisor
-    such that
-    the result<=threshold==>
-    take ceil value=7/3=3,10/2=5
+    ==> Find the smallest divisor such that the result<=threshold==>take ceil value=7/3=3,10/2=5
 
-    1)
-    As divisor incr,
-    the sum
-    decr so, we
-    need to
-    find sum<=
-    threshold
+    1)As divisor incr,the sumdecr so, we need to find sum <= threshold
 
-Find
-    what shows
-    monotonic behavior
-    that is divisor,
-    As the
-    divisor increases
-    the sum decreases.So,
-    minimum divisor
-    and maximum
-    divisor choose and do
-    binary search
-    on that
-    range to
-    find the
-    smalllest divisor
-    less than
-    or equal
-    to threshold
+    Find what shows monotonic behavior that is divisor,
+    As the divisor increases the sum decreases.So, minimum divisor and maximum divisor choose and do binary search
+    on that range to find the smalllest divisor less than  or equal to threshold
 
-    nums=[1,2,5,9]mini=Integer.MAX_VALUE;maxi=Integer.MIN_VALUE;for(
-    int i = 0;i<nums.length;i++)
+    nums=[1,2,5,9]
+
+    mini=Integer.MAX_VALUE;
+    maxi=Integer.MIN_VALUE;
+
+    for(int i = 0;i<nums.length;i++)
     {
         mini = Math.min(mini, nums[i]);//1
         maxi = Math.max(maxi, nums[i]);//9
 
 }
-This gives
-us divisor
-range from 1 to 9:
+This gives us divisor range from 1 to 9:
 minm divisor:1
 maximum divisor is:9
 
-basically positive
-integer divisor
-divide all
-the arrray
-by it, and
-the sum
-the division
-results
+basically positive integer divisor divide all the arrray by it, and
+the sum the division results
 */
