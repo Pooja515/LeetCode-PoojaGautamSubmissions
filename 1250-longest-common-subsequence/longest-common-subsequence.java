7225ab8d1e692a9efa2db8f3d4e2064a051d4f69
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-07 22:31:55 +0530

    Time: 23 ms (40.18%) | Memory: 53.8 MB (70.14%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,24 +1,23 @@
 class Solution {
-    int[][] memo;
+
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length() , n= text2.length();
-        memo = new int[m][n];
-        for(int[] rows : memo){
-            Arrays.fill(rows,-1);
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int ind1 = 0; ind1 <= m; ind1++) {
+            for (int ind2 = 0; ind2 <= n; ind2++) {
+                if (ind1 == 0 || ind2 == 0) {
+                    dp[ind1][ind2] = 0;
+                } else {
+                    // match
+                    if (text1.charAt(ind1 - 1) == text2.charAt(ind2 - 1)) {
+                        dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                    } else
+                        //not match
+                        dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                }
+            }
         }
-
-        return f(m-1,n-1,text1,text2);
-    }
-    int f(int ind1 ,int ind2 , String s1 , String s2){
-        if(ind1 < 0 || ind2 < 0) return 0;
-
-        if(memo[ind1][ind2] != -1) return memo[ind1][ind2];
-
-        // match
-        if(s1.charAt(ind1) == s2.charAt(ind2)){
-            return memo[ind1][ind2] = 1 + f(ind1-1,ind2-1,s1,s2);
-        }
-        //not match
-        return memo[ind1][ind2] = Math.max(f(ind1-1,ind2,s1,s2) , f(ind1,ind2-1,s1,s2));
+      return dp[m][n];
     }
 }
