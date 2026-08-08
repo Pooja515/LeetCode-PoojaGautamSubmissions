50f1726f213241f779d4aadfe86a89e0ea7ff94d
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-08 16:54:00 +0530

    Time: 18 ms (76.09%) | Memory: 54.4 MB (52.92%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,27 +1,26 @@
 class Solution {
-    int[][] memo;
+
     public int numDistinct(String s, String t) {
         int m = s.length() , n= t.length();
         if(m <n) return 0;
 
-        memo= new int[m][n];
-        for(int[] rows:memo){
-            Arrays.fill(rows,-1);
+        int[][] dp= new int[m+1][n+1];
+
+        for(int ind1 =0 ;ind1 <= m;ind1++){
+            dp[ind1][0] = 1;
         }
 
-        return f(m-1,n-1,s,t);
+        for(int ind1=1;ind1 <= m; ind1++){
+            for(int ind2 = 1 ; ind2 <= n ; ind2++){
 
-    }
-    int f(int ind1,int ind2,String s1,String s2){
-        if(ind2 <0) return 1;
-        if(ind1 < 0) return 0;
-
-        if(memo[ind1][ind2] != -1) return memo[ind1][ind2];
-
-        //match 
-        if(s1.charAt(ind1) == s2.charAt(ind2))
-              return memo[ind1][ind2]= f(ind1-1,ind2-1,s1,s2) + f(ind1-1,ind2,s1,s2);
-        
-        return memo[ind1][ind2]=f(ind1-1,ind2,s1,s2);
+               if(s.charAt(ind1-1) == t.charAt(ind2-1)){
+                    dp[ind1][ind2]= dp[ind1-1][ind2-1] + dp[ind1-1][ind2];
+               }
+                else{
+                      dp[ind1][ind2]=dp[ind1-1][ind2];
+               }
+            }
+        }
+    return dp[m][n];
     }
 }
