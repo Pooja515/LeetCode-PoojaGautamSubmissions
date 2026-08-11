65a68688674fd3574373e0e84f297f669338aa32
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-11 16:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.31%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,24 @@
 class Solution {
-  
+    int[] memo ;
     public int rob(int[] nums) {
-        int n = nums.length;
+       int n = nums.length;
+       memo = new int[n];
+       Arrays.fill(memo,-1);
+       return f(n-1,nums); 
+    }
+    int f(int i , int[] nums){
+        //base case
+        if(i == 0 ) return nums[i];
+        if(i==1) return Math.max(nums[0],nums[1]);
 
-        if(n==1) return nums[0];
+        if(memo[i] != -1) return memo[i];
 
-        int prev2 = nums[0] , prev1 = Math.max(nums[0] , nums[1]);
-        for(int i = 2;i<n ; i++){
-                int pick = nums[i] + prev2;
-                int notpick = 0 + prev1;
-
-                int cur = Math.max(pick , notpick);
-                prev2 = prev1;
-                prev1 = cur;
+        // notake 
+        int nottake = 0 + f(i-1,nums);
+        int take = 0;
+        if(i>1){
+            take = nums[i] + f(i-2,nums);
         }
-    return prev1;
+        return memo[i] = Math.max(take,nottake);
     }
 }
