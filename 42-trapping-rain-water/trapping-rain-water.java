6bc9ceb68e859d8e241a0399aa03b72a009b63b2
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-13 00:29:57 +0530

    Time: 1 ms (58.71%) | Memory: 47.8 MB (45.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,20 +1,19 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int l = 0 , r = n-1;
-        int lmax = 0 , rmax = 0 , trap =0;
-        while(l < r){
-            lmax = Math.max(lmax , height[l]);
-            rmax = Math.max(rmax , height[r]);
-            if(lmax < rmax){
+        int trap = 0, lmax = 0, rmax = 0, l = 0, r = n - 1;
+        for (int i = 0; i < n; i++) {
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+            if (lmax < rmax) {
                 trap += lmax - height[l];
-                l++; 
-            }
-            else{
+                l++;
+            } else {
                 trap += rmax - height[r];
                 r--;
             }
         }
         return trap;
+
     }
 }
