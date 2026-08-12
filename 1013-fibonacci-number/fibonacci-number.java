59d422dcb4b5fbbaece446ecf167651c138775d8
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-13 01:25:56 +0530

    Time: 9 ms (43.29%) | Memory: 41.9 MB (70.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,17 +1,7 @@
 class Solution {
-    int[] memo;
     public int fib(int n) {
         if(n<=1) return n;
-        memo = new int[n+1];
-        Arrays.fill(memo , -1);
-
-        return f(n);
-    }
-    int f(int n){
-        if(n <= 1) return n;
-
-        if(memo[n] != -1) return memo[n];
-
-        return memo[n] = f(n-1)+f(n-2);
+        
+        return fib(n-1) + fib(n-2);
     }
 }
