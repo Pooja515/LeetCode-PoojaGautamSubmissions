77e1b66e99faf2bdacd4c7a81d5c2b7091d1345b
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-13 01:30:59 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/java/49-GroupAnagrams_20260813_013058.java
@@ -0,0 +1,17 @@
+// Last updated: 13/08/2026, 01:30:58
+1class Solution {
+2    public int fib(int n) {
+3        if(n <= 1) return n;
+4        int[] memo = new int[n+1];
+5        Arrays.fill(memo , -1);
+6        
+7        return f(n,memo);
+8    }
+9    int f(int n , int[] memo){
+10        if(n <= 1) return n;
+11
+12        if(memo[n] != -1) return memo[n];
+13
+14        return memo[n] = f(n-1 , memo) + f(n-2,memo);
+15    }
+16}
