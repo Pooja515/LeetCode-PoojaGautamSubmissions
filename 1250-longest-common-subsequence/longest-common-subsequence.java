49ef002bab6c3268eb6811bdd59739b086e89b69
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-13 21:53:11 +0530

    Time: 27 ms (22.44%) | Memory: 53.8 MB (69.76%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,25 +1,28 @@
 class Solution {
-
+    int[][] memo;
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n = text2.length();
-        int[] dp = new int [n + 1];
 
-        for (int ind1 = 0; ind1 <= m; ind1++) {
-             int[] cur = new int [n + 1];
-            for (int ind2 = 0; ind2 <= n; ind2++) {
-                if (ind1 == 0 || ind2 == 0) {
-                    cur[ind2] = 0;
-                } else {
-                    // match
-                    if (text1.charAt(ind1 - 1) == text2.charAt(ind2 - 1)) {
-                        cur[ind2] = 1 + dp[ind2 - 1];
-                    } else
-                        //not match
-                        cur[ind2] = Math.max(dp[ind2], cur[ind2 - 1]);
-                }
-            }
-            dp=cur;
+        memo= new int[m][n];
+        for(int[] rows: memo){
+            Arrays.fill(rows,-1);
         }
-        return dp[n];
+
+        return f(m - 1, n - 1, text1, text2);
+    }
+
+    int f(int ind1, int ind2, String s1, String s2) {
+        // base case 
+        if (ind1 < 0 || ind2 < 0)
+            return 0;
+
+        if(memo[ind1][ind2] != -1) return memo[ind1][ind2];
+
+
+        if (s1.charAt(ind1) == s2.charAt(ind2))
+            return memo[ind1][ind2] = 1 + f(ind1 - 1, ind2 - 1, s1, s2);
+
+        return  memo[ind1][ind2] = Math.max(f(ind1 - 1, ind2, s1, s2), f(ind1, ind2 - 1, s1, s2));
+
     }
 }
