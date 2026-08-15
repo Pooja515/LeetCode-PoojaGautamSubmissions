0f8b7ffa2a2eb012649549f3fc13439602c6e222
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-15 18:21:25 +0530

    Time: 15 ms (82.99%) | Memory: 46.9 MB (35.42%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,40 +1,31 @@
 class Solution {
-    int[][] memo;
+
     public int coinChange(int[] coins, int amount) {
-        int n= coins.length;
-        memo= new int[n][amount+1];
+        int n = coins.length;
+        int[] dp = new int [amount + 1];
 
-        for(int[] rows:memo){
-            Arrays.fill(rows,-1);
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) {
+                dp[t] = t / coins[0];
+            } else
+                dp[t] = (int) 1e9;
+
         }
 
-        if(amount == 0) return 0;
-        if(n==1){
-            if(amount % coins[0] == 0){
-                return amount/coins[0];
+        for (int ind = 1; ind < n; ind++) {
+             int[] cur = new int [amount + 1];
+            for (int t = 0; t <= amount; t++) {
+                //nottake
+                int nottake = 0 + dp[t];
+                int take = (int) 1e9;
+                if (coins[ind] <= t) {
+                    take = 1 + cur[t - coins[ind]];
+                }
+
+                cur[t] = Math.min(take, nottake);
             }
-            else
-                 return -1;
+            dp=cur;
         }
-        
-        return f(0,amount ,coins) >= (int) 1e9 ?-1:f(0,amount ,coins);
-    }
-    int f(int ind ,int amount , int[] coins){
-
-        //base case 
-        if(amount == 0) return 0;
-
-        if(ind == coins.length) return (int) 1e9;
-
-        if(memo[ind][amount] != -1) return memo[ind][amount];
-
-        //nottake
-        int nottake = 0 + f(ind+1,amount,coins);
-        int take = (int) 1e9;
-        if(coins[ind] <= amount){
-            take = 1 + f(ind,amount - coins[ind] , coins);
-        }
-
-        return memo[ind][amount] = Math.min(take,nottake);
+        return dp[amount] >= (int) 1e9 ?-1:dp[amount];
     }
 }
