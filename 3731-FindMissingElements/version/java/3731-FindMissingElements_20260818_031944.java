782395174df82f4e6e6e2ca04483d2f12f9f7efb
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-18 03:19:44 +0530

    Create 3731-FindMissingElements

---
+++ 3731-FindMissingElements/version/java/3731-FindMissingElements_20260818_031944.java
@@ -0,0 +1,24 @@
+// Last updated: 18/08/2026, 03:19:44
+1class Solution {
+2    public List<Integer> findMissingElements(int[] nums) {
+3         List<Integer> ans = new ArrayList<>();
+4         Set<Integer> set = new HashSet<>();
+5
+6         int mini=Integer.MAX_VALUE;
+7         int maxi = Integer.MIN_VALUE;
+8
+9         for(int num : nums){  //O(N)
+10            mini = Math.min(mini ,num);
+11            maxi= Math.max(maxi,num);
+12            set.add(num);
+13         }
+14
+15         for(int i = mini ;i <= maxi; i++){ // O(K)
+16            if(!set.contains(i)) ans.add(i);
+17         }
+18
+19         return ans;
+20    }
+21}
+22//Time complexity : O(N * K)// The second loop runs K times (from mini to maxi).
+23// Space Complexity :O(N)
