bc9d14767d57dc6ba5f2f2c9212fc8163e3529fc
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-18 03:19:46 +0530

    Time: 3 ms (83.91%) | Memory: 46.9 MB (44.77%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -6,16 +6,18 @@ class Solution {
          int mini=Integer.MAX_VALUE;
          int maxi = Integer.MIN_VALUE;
 
-         for(int num : nums){  
+         for(int num : nums){  //O(N)
             mini = Math.min(mini ,num);
             maxi= Math.max(maxi,num);
             set.add(num);
          }
 
-         for(int i = mini ;i <= maxi; i++){
+         for(int i = mini ;i <= maxi; i++){ // O(K)
             if(!set.contains(i)) ans.add(i);
          }
 
          return ans;
     }
 }
+//Time complexity : O(N * K)// The second loop runs K times (from mini to maxi).
+// Space Complexity :O(N)
