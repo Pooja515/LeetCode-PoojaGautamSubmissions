1e50d802c11813fd17c2bd3a629d859afe7ddb79
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-25 03:38:47 +0530

    Time: 1 ms (17.34%) | Memory: 43.5 MB (46.01%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,35 +1,42 @@
 class Solution {
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid == null || obstacleGrid.length == 0 ) return 0;
-        int m= obstacleGrid.length , n= obstacleGrid[0].length;
+        if (obstacleGrid == null || obstacleGrid.length == 0)
+            return 0;
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
 
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
+            return 0;
 
         int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j] =1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
                 }
 
-                else{
+                else {
 
-                    if(i<0 || j<0) dp[i][j] = 0;
-                    if(obstacleGrid[i][j] == 1) {
+                    if (i < 0 || j < 0) {
+                        dp[i][j] = 0;
+                        continue;
+                    }
+                    if (obstacleGrid[i][j] == 1) {
                         dp[i][j] = 0;
                         continue;
                     }
 
-                    int up =0,left =0;
-                    if(i > 0) up = dp[i-1][j];
-                    if(j > 0) left = dp[i][j-1];
+                    int up = 0, left = 0;
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    if (j > 0)
+                        left = dp[i][j - 1];
 
-                    dp[i][j] = up+left;
-                            }
-                        }
-                    }
-       return dp[m-1][n-1]; 
-    
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+
     }
 }
