03b384678ad74a95e9a3957a0677ee5437b3593f
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-25 11:23:36 +0530

    Time: 13 ms (82.07%) | Memory: 47.8 MB (32.94%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -6,21 +6,20 @@ class Solution {
         
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
 
-        boolean[][] visited = new boolean[n][n];
-
+      
         q.offer(new int[] {0,0,1});
-        visited[0][0] = true;
+        grid[0][0]=1;
 
       int[][] directions ={{-1,0},{1,0},{0,-1},{0,1},{-1,-1},{-1,1},{1,1},{1,-1}};
       while(!q.isEmpty()){
         int[] curr  = q.poll();
         int r = curr[0] , c = curr[1], dist = curr[2];
-          if(r == n-1 && c ==n-1) return dist;
+        if(r == n-1 && c ==n-1) return dist;
         for(int[] d:directions){
             
               int newr = r +d[0] , newc= c+d[1];
-              if(newr >=0 && newr<n && newc>=0 && newc < n && grid[newr][newc] == 0 && !visited[newr][newc] ){
-                visited[newr][newc] = true;
+              if(newr >=0 && newr<n && newc>=0 && newc < n && grid[newr][newc] == 0 ){
+                grid[newr][newc] = 1;
                 q.offer(new int[] {newr , newc ,dist+1});
               }
         }
