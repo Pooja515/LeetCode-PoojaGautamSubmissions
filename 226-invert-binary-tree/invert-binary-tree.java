72562d43db39494f9c1a0e24b4c88cd985839966
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-25 12:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (11.23%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -21,12 +21,8 @@ class Solution {
             root.left = root.right;
             root.right = temp;
         
-        if(root.left != null){
-            TreeNode left= invertTree(root.left);
-        }
-        if(root.right != null){
-           TreeNode right = invertTree(root.right);
-        }
+         invertTree(root.left);
+         invertTree(root.right);
         
 
         return root;
