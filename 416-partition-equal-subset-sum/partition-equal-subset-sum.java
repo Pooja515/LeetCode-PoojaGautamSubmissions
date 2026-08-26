288c723d057c9b3d988ad8f3c83b734fbbec6427
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-26 11:05:15 +0530

    Time: 96 ms (20.93%) | Memory: 75 MB (22.87%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,37 +1,41 @@
 class Solution {
-    
+    Boolean[][] memo ;
     public boolean canPartition(int[] nums) {
-        int totalsum = 0;
-        for(int num : nums){
-            totalsum += num;
+
+        int n = nums.length, totalsum = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalsum += nums[i];
         }
+        if (totalsum % 2 != 0)
+            return false;
 
-        if(totalsum % 2 != 0) return false;
+        int target = totalsum / 2;
 
-        int target = totalsum / 2 ;
-        int n = nums.length;
+         memo = new Boolean [n][target+1];
 
-        boolean[][] dp = new boolean [n][target + 1];
-        
-          //// Target 0 can always be formed by selecting nothing.
-          for(int ind = 0 ;ind < n; ind++){
-            dp[ind][0] = true;
-          }
 
-       for(int ind = 1 ;ind < n; ind++){
-        for(int t = 1 ;t <= target ; t++){
-            
-            boolean notpick = dp[ind-1][t];
-            boolean pick = false ;
-            if(nums[ind] <= t){
-                pick = dp[ind-1][t - nums[ind]];
-            }
-             dp[ind][t] = pick || notpick;
-        }
-      
-    
-       }
-    
-    return dp[n-1][target];
+        return f(n - 1, target, nums);
     }
+
+    boolean f(int i, int target, int[] nums) {
+        if (target == 0){
+             return true;
+        }
+        if(i == 0){
+            if(nums[i] <= target ) return target == nums[i];
+            else
+                 return false;
+        }
+    
+    if(memo[i][target] != null) return memo[i][target];
+    boolean nottake = f(i - 1, target, nums);
+    boolean take = false;
+    if(nums[i] <= target){
+        take = f(i - 1, target - nums[i], nums);
+    }
+
+    return memo[i][target] = take||nottake;
+
+}
 }
