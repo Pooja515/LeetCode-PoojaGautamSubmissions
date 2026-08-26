fa37cb8cbb9cfb180a36e4326d302d76bf6ec215
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-26 12:36:41 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/version/java/416-PartitionEqualSubsetSum_20260826_123640.java
@@ -0,0 +1,42 @@
+// Last updated: 26/08/2026, 12:36:40
+1class Solution {
+2
+3    public boolean canPartition(int[] nums) {
+4
+5        int n = nums.length, totalsum = 0;
+6
+7        for (int i = 0; i < n; i++) {
+8            totalsum += nums[i];
+9        }
+10        if (totalsum % 2 != 0)
+11            return false;
+12
+13        int target = totalsum / 2;
+14
+15        boolean [][] dp = new boolean [n][target+1];
+16
+17        for(int i =0;i<n;i++){
+18            dp[i][0] = true;
+19        }
+20
+21        if(n==1){
+22            if(nums[0] <= target) return dp[0][nums[0]];
+23            else
+24                  dp[0][target] = false;
+25        }
+26    
+27    for(int i=1;i<n;i++){
+28        for(int t=0;t<= target;t++){
+29              boolean nottake = dp[i - 1][t];
+30              boolean take = false;
+31              if(nums[i] <= t){
+32                 take = dp[i - 1][t - nums[i]];
+33                }
+34
+35               dp[i][t] = take||nottake;
+36        }
+37    }
+38  
+39     return dp[n-1][target];
+40    }
+41}
