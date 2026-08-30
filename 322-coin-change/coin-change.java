fe6c1021dda64f14927ccfbf5b97591d683338d7
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-31 00:38:21 +0530

    Time: 33 ms (16.85%) | Memory: 47.1 MB (25.98%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,31 +1,31 @@
 class Solution {
-
+    int[][] memo;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[] dp = new int [amount + 1];
+        memo = new int[n][amount +1];
+        for(int[] rows : memo){
+            Arrays.fill(rows,-1);
+        }
 
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0) {
-                dp[t] = t / coins[0];
+        int res =f(n - 1, amount, coins);
+
+        return res >= (int) 1e9?-1:res;
+    }
+
+    int f(int i , int amount , int[] coins){
+        if(amount == 0) return 0;
+        if (i == 0) {
+            if (amount % coins[0] == 0) {
+                return amount / coins[0];
             } else
-                dp[t] = (int) 1e9;
-
+                return (int) 1e9;
         }
-
-        for (int ind = 1; ind < n; ind++) {
-             int[] cur = new int [amount + 1];
-            for (int t = 0; t <= amount; t++) {
-                //nottake
-                int nottake = 0 + dp[t];
-                int take = (int) 1e9;
-                if (coins[ind] <= t) {
-                    take = 1 + cur[t - coins[ind]];
-                }
-
-                cur[t] = Math.min(take, nottake);
-            }
-            dp=cur;
+        if(memo[i][amount] != -1) return memo[i][amount];
+        int nottake = 0 + f(i-1,amount,coins);
+        int take =(int) 1e9;
+        if(coins[i] <= amount ){
+            take = 1 + f(i,amount-coins[i] ,coins);                                 
         }
-        return dp[amount] >= (int) 1e9 ?-1:dp[amount];
+        return memo[i][amount]= Math.min(take,nottake);
     }
 }
