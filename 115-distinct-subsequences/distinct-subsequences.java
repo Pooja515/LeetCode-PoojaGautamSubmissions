c382c0fdc6179c19bc10863efee4e1d9aac32aeb
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-06 16:26:29 +0530

    Time: 20 ms (53.25%) | Memory: 54.4 MB (38.68%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,29 +1,34 @@
 class Solution {
-    int[][] memo;
+
     public int numDistinct(String s, String t) {
-        int m = s.length() , n= t.length();
-        if(m < n) return 0;
-        memo= new int[m][n];
-        for(int[] rows : memo){
-            Arrays.fill(rows,-1);
+        int m = s.length(), n = t.length();
+        if (m < n)
+            return 0;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if(j==0) {
+                    dp[i][j] =1;
+                    continue;
+                }
+                if(i==0){
+                    dp[i][j] =0;
+                    continue;
+                }
+                int take = 0, nottake = 0;
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    take = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    nottake = dp[i - 1][j];
+                }
+
+                dp[i][j] = take + nottake;
+            }
+
         }
 
-        return f(m-1,n-1,s,t);
-    }
-    int f(int i,int j,String s, String t){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        int take =0 ,nottake =0;
+        return dp[m][n];
 
-        if(memo[i][j] != -1) return memo[i][j];
-      
-        if(s.charAt(i) == t.charAt(j)){
-            take =f(i-1, j-1,s,t) + f(i-1,j,s,t);
-        }
-        else 
-               nottake= f(i-1,j,s,t);
-
-        return memo[i][j] = take + nottake;
-       
     }
 }
