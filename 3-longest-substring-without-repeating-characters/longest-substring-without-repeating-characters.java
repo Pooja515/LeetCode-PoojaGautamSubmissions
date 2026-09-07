bead3f4b7d4eaa6d62f560baa7a49a37e72db67b
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-08 00:19:56 +0530

    Time: 41 ms (50.14%) | Memory: 47.9 MB (38.57%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,14 +1,15 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-       HashMap<Character ,Integer> map = new HashMap<>();
-       int l = 0 , maxlen = 0; 
-       for(int r = 0 ; r < s.length() ; r++){
-          if(map.containsKey(s.charAt(r))){
-             l = Math.max(l , map.get(s.charAt(r))+1);
-          }
-          map.put(s.charAt(r) , r);
-          maxlen = Math.max(maxlen , r-l+1);
-       } 
-       return maxlen;
+        HashMap<Character,Integer> map = new HashMap<>();
+        int l=0,maxi=0;
+        for(int r=0;r<s.length();r++){
+            if(map.containsKey(s.charAt(r))){
+                l= Math.max(l,map.get(s.charAt(r))+1);
+            }
+            maxi=Math.max(maxi,r-l+1);
+            map.put(s.charAt(r),r);
+            
+        }
+        return maxi;
     }
 }
