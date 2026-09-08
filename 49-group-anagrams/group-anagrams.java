dcaf7166decb675e6c18a6426ef15482c08efa22
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-09 01:46:10 +0530

    Time: 18 ms (20.45%) | Memory: 50.2 MB (15.22%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,25 +1,27 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-      HashMap<String , List<String>> map = new HashMap<>();
-      for(String str : strs){
-        int n= str.length();
-        int[] freq = new int[26];
+        List<List<String>> ans = new ArrayList<>();
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String str:strs){
+            int[] freq = new int[26];
 
-        for(int i =0;i<n;i++){
-            freq[str.charAt(i)-'a']++;
+            for(int i=0;i<str.length();i++){
+                freq[str.charAt(i) -'a']++;
+            }
+          
+            StringBuilder sb = new StringBuilder();
+            for(int num : freq){
+                sb.append(num);
+                sb.append('#');
+            }
+
+            String key = sb.toString();
+
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(str);
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0;i<26;i++){
-            sb.append(freq[i]);
-            sb.append("#");
-        }
-        String key = sb.toString();
-        map.putIfAbsent(key,new ArrayList<>());
-        map.get(key).add(str);
-
-      }
-     return new ArrayList<>(map.values());
+        return new ArrayList<>(map.values());
+        
     }
 }
