0e6e64d4eba14d2e79172b359dc880c870bf041a
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-09 00:45:37 +0530

    Time: 1 ms (96.63%) | Memory: 47.8 MB (24.56%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,51 +1,46 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean rowflag = false , colflag = false; 
-        int m = matrix.length , n = matrix[0].length;
-//explore 0 column , (if column 0 has a zero)
-        for(int i =0 ; i<m;i++){
-            if(matrix[i][0] == 0){
-                colflag = true;
-                break;
-            }
-        }
-// explore 0 row , (if row 0 has a zero)
-        for(int i =0 ; i<n;i++){
-            if(matrix[0][i] == 0){
-                rowflag = true;
-                break;
-            }
-        }
+        int m= matrix.length, n= matrix[0].length;
 
-        for(int i=1;i<m;i++){
-            for(int j=1 ; j<n; j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] =0;
-                    matrix[0][j] =0;
-    
+        boolean rowflag= false , colflag =false;
+
+        for(int r=0;r<m;r++){
+            if(matrix[r][0] == 0 ) {
+                colflag =true;
+                break;
+            }
+        }
+        for(int c=0;c<n;c++){
+            if(matrix[0][c] == 0) {
+                rowflag =true;
+                break;
+            }
+        }
+        for(int r=1;r<m;r++){
+            for(int c=1;c<n;c++){
+                if(matrix[r][c] == 0){
+                    matrix[0][c] = 0;
+                    matrix[r][0] = 0;
                 }
             }
         }
 
-        for(int i=1;i<m;i++){
-            for(int j=1 ; j<n; j++){
-                if( matrix[i][0] ==0 || matrix[0][j] ==0){
-                    matrix[i][j] =0;
+        for(int r=1;r<m;r++){
+            for(int c=1;c<n;c++){
+                if(matrix[0][c] ==0 || matrix[r][0] ==0){
+                    matrix[r][c] =0;
                 }
             }
         }
-// if row 0 has 0 , make entire row 0 to 0
+
         if(rowflag){
-            for(int i =0 ; i<n ;i++){
-                matrix[0][i] =0;
-            
+            for(int c=0;c<n;c++){
+                matrix[0][c] =0;
             }
         }
-// if col 0 has 0 , make entire col 0 to 0
          if(colflag){
-            for(int i =0 ; i<m ;i++){
-                matrix[i][0] =0;
-            
+            for(int r=0;r<m;r++){
+                matrix[r][0] =0;
             }
         }
     }
