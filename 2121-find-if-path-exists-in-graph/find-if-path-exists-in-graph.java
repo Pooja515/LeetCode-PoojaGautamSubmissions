1a3ffcbb5ee56fedf1bddd7bb5ba701229882a7a
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-13 03:12:58 +0530

    Time: 184 ms (13.84%) | Memory: 280.5 MB (64.90%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -2,25 +2,26 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> adj = new ArrayList<>();
         boolean[] visited = new boolean[n];
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int[] edge: edges){
+        for (int[] edge : edges) {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
 
-        return dfs(source,destination,adj,visited);
+        return dfs(source, destination, adj, visited);
     }
-    boolean dfs(int node ,int destination , List<List<Integer>> adj, boolean[] visited ){
-      
-        if(node == destination) return true;
-        visited[node] = true;
-        for(int neig : adj.get(node)){
-            if(!visited[neig]){
-               if( dfs(neig,destination,adj,visited)) return true;
+
+    boolean dfs(int source, int destination, List<List<Integer>> adj, boolean[] visited) {
+        if (source == destination)
+            return true;
+        visited[source] = true;
+        for (int neigh : adj.get(source)) {
+            if (!visited[neigh]) {
+                if (dfs(neigh, destination, adj, visited))
+                    return true;
             }
-           
         }
         return false;
     }
