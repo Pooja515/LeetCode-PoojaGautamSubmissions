741858b74ccfc3b4a3f65b826fb250902255dff8
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-14 00:27:51 +0530

    Time: 4 ms (46.01%) | Memory: 52.4 MB (32.58%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,32 +1,32 @@
 class Solution {
+    int[][] dirc = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0)
             return 0;
-        int m = grid.length, n = grid[0].length, island = 0;
-        boolean[][] visited = new boolean[m][n];
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == '1' && !visited[r][c]) {
-                    dfs(r, c, grid, visited);
-                    island++;
+        int m = grid.length, n = grid[0].length;
 
+        boolean[][] visited = new boolean[m][n];
+        int island = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(i, j, grid, visited, m, n);
+                    island++;
                 }
             }
         }
         return island;
+
     }
 
-    int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
-    void dfs(int r, int c, char[][] grid, boolean[][] visited) {
+    void dfs(int r, int c, char[][] grid, boolean[][] visited, int m, int n) {
         visited[r][c] = true;
-        for (int[] d : dir) { // explore neighbors
-            int newr = r + d[0], newc = c + d[1];
-            if (newr >= 0 && newr < grid.length && newc >= 0 && newc < grid[0].length && grid[newr][newc] == '1'
-                    && !visited[newr][newc]) {
-                dfs(newr, newc, grid, visited);
+        for (int[] dir : dirc) {
+            int newr = r + dir[0], newc = c + dir[1];
+            if (newr >= 0 && newr < m && newc >= 0 && newc < n && grid[newr][newc] == '1' && !visited[newr][newc]) {
+                dfs(newr, newc, grid, visited, m, n);
             }
         }
-
     }
 }
