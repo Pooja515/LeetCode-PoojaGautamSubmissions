8ecd2cfc95b1d6b7f5b39b0d5e1fad800536720e
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-14 01:54:07 +0530

    Time: 3 ms (24.88%) | Memory: 46.7 MB (21.67%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,28 +1,41 @@
 class Solution {
-    int[][] dir = {{-1,0},{1,0} , {0,-1},{0,1}};
+    int[][] dirc = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
     public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = 0;
-        int m = grid.length , n = grid[0].length;
-        if(grid == null || m==0) return 0;
+      
+        if (grid == null || grid.length == 0)
+            return 0;
+        int m = grid.length, n = grid[0].length;
+
         boolean[][] visited = new boolean[m][n];
-        for(int r=0;r<m;r++){
-            for(int c=0;c<n;c++){
-                 if(grid[r][c] == 1 && !visited[r][c]){
-                    maxArea = Math.max(maxArea , dfs(r,c,grid,visited,m,n));
-                 } 
+        int maxArea= 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                   maxArea = Math.max(dfs(i, j, grid, visited, m, n), maxArea);
+                   
+                }
             }
         }
         return maxArea;
+
     }
-    int dfs(int r,int c,int[][] grid , boolean[][] visited ,int m ,int n){
-        int area =1;
+
+    int dfs(int r, int c, int[][] grid, boolean[][] visited, int m, int n) {
         visited[r][c] = true;
-        for(int[] d : dir){
-            int newr = r + d[0] , newc = c + d[1];
-            if(newr >= 0 && newr <m && newc >= 0 && newc <n && grid[newr][newc] == 1 && !visited[newr][newc]){
-                area += dfs(newr , newc , grid, visited , m , n);
+        int island =1;
+        for (int[] dir : dirc) {
+            int newr = r + dir[0], newc = c + dir[1];
+            if (newr >= 0 && newr < m && newc >= 0 && newc < n && grid[newr][newc] == 1 && !visited[newr][newc]) {
+                island += dfs(newr, newc, grid, visited, m, n);
             }
-        }
-        return area;
+        } 
+        return island; 
     }
 }
+
+
+
+
+   
+    
