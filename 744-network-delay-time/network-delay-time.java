7a88e95e6681948939e7d112e0ecdc8323245ea6
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-18 01:57:10 +0530

    Time: 11 ms (56.97%) | Memory: 48.5 MB (91.85%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,58 +1,52 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-
         List<List<int[]>> adj = new ArrayList<>();
 
-        for(int i = 0; i<=n ; i++){
+        for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int[] time : times){
-            int u = time[0];
-            int v = time[1];
-            int wt = time[2];
+        for(int[] time:times){
+            int u=time[0];
+            int v=time[1];
+            int t=time[2];
 
-            adj.get(u).add(new int[] {v,wt});
-
+            adj.get(u).add(new int[] {v,t});
+           // adj.get(v).add(new int[] {u,t});
         }
 
-        int[] distance = new int[n+1];
-        Arrays.fill(distance , (int) 1e9);
-        distance[k] =0;
+        int[] traveltime = new int[n+1];
+        Arrays.fill(traveltime,Integer.MAX_VALUE);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
+        traveltime[k]=0;
 
-       //pair {distance ,src}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> Integer.compare(a[0],b[0]));
         pq.offer(new int[] {0,k});
 
         while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int currdistance = curr[0];
-            int node = curr[1];
-            
-            if(currdistance > distance[node]) continue;
+            int[] cur = pq.poll();
+            int time = cur[0];
+            int node = cur[1];
 
+            if(time > traveltime[node]) continue;
 
-            for(int[] neighbors : adj.get(node)){
-                int neighbor = neighbors[0];
-                int w = neighbors[1];
+            for(int[] edge: adj.get(node) ){
+                int neighbor = edge[0];
+                int tt = edge[1];
 
-                int newdistance = currdistance + w;
-                if(newdistance < distance[neighbor]){
-                    distance[neighbor] = newdistance;
-                    pq.offer(new int[] {newdistance , neighbor});
+                int newtime = time +tt;
+                if(newtime < traveltime[neighbor]){
+                    traveltime[neighbor] = newtime;
+                    pq.offer(new int[] {newtime,neighbor});
                 }
             }
         }
+        int maxtime=0;
+        for(int i=1 ;i<=n;i++){
+            if(traveltime[i] == Integer.MAX_VALUE) return -1;
+            maxtime=Math.max(maxtime,traveltime[i]);
 
-        int maxi=0;
-        for(int i=1;i<=n;i++){
-            if(distance[i] == (int) 1e9 ){
-                return -1;
-            }
-            maxi = Math.max(maxi,distance[i]);
         }
-        return maxi;
-
+        return maxtime;
     }
 }
