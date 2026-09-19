e7eb9fed512886f4542808bb5c6dc710ece651f0
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-20 03:46:25 +0530

    Time: 6 ms (9.44%) | Memory: 50.3 MB (56.98%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,34 +1,35 @@
 class Solution {
-    int[][] memo;
+
     public int minPathSum(int[][] grid) {
 
         if (grid == null || grid.length == 0)
             return 0;
         int m = grid.length, n = grid[0].length;
 
-        memo = new int[m][n];
-        for(int[] rows : memo){
-            Arrays.fill(rows,(int) 1e9);
-        }
+        int[][] dp = new int[m + 1][n + 1];
 
-        return f(m - 1, n - 1, grid);
-
-    }
-
-    int f(int i, int j, int[][] grid) {
-
-        // base case 1
-        if (i == 0 && j == 0)
-            return grid[i][j];
-        // base case 2
-        if (i < 0 || j < 0)
-            return (int) 1e9;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // base case 1
+                if (i == 0 && j == 0){
+                    dp[i][j]= grid[i][j];
+                    continue;
+            }
+                // base case 2
+                if (i <0 || j < 0){
+                   dp[i][j]= (int) 1e9;
+                   continue;
+                }
         
-        if(memo[i][j] != (int) 1e9) return memo[i][j];
 
-        int up = f(i - 1, j, grid);
-        int left = f(i, j - 1, grid);
+                int up =(int) 1e9;
+                if(i>0) up = dp[i - 1][j];
+                int left = (int) 1e9;
+                if(j>0) left = dp[i][j - 1];
 
-        return memo[i][j] =grid[i][j] + Math.min(up, left);
+                dp[i][j] = grid[i][j] + Math.min(up, left);
+            }
+        }
+       return dp[m-1][n-1];
     }
 }
