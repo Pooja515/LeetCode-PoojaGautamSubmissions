c60192c33cdda1126569987c786361b666570a1f
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-25 10:34:17 +0530

    Time: 32 ms (18.63%) | Memory: 47.2 MB (17.93%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,39 +2,36 @@ class Solution {
     int[][] memo;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        memo=new int[n][amount+1];
-        for(int[] rows: memo){
+        memo = new int[n][amount+1];
+
+        for(int[] rows:memo){
             Arrays.fill(rows,-1);
         }
 
-        int res = f(n - 1, amount, coins);
+        int res = f(n-1,amount,coins);
 
-        return res >= (int) 1e9 ? -1 : res;
+        return  res >= (int) 1e9 ? -1:res;
     }
 
-    int f(int i, int target, int[] coins) {
+    int f(int i,int target,int[] coins){
 
-        if (target == 0)
-            return 0;
-        if (i == 0) {
-
-            if (target % coins[0] == 0) {
-                return target / coins[0];
-            }
-
+        if(target == 0) return 0;
+        if(i==0) {
+            if(target % coins[0] == 0) 
+                 return target/coins[0];
             else
-                return (int) 1e9;
-
+                 return (int) 1e9;
         }
-
         if(memo[i][target] != -1) return memo[i][target];
-
-        int nottake = f(i - 1, target, coins);
+        //nottake 
+        int nottake = 0 + f(i-1,target,coins);
         int take = (int) 1e9;
-        if (coins[i] <= target) {
-            take = 1 + f(i, target - coins[i], coins);
+        if(coins[i] <= target){
+            take = 1 + f(i,target-coins[i],coins);
         }
 
-        return memo[i][target] = Math.min(take, nottake);
+        return memo[i][target] = Math.min(take,nottake);
     }
+
+
 }
